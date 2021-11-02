3a9eb8071dee2870f49ff70a50e628a2d9b79f65
Viktoriia Gorodetska <vikagoro3@gmail.com>
2021-11-02 13:14:29 +0200

    add_button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Sidebar from './components/Sidebar'
+import Header from './components/Header'
+import Line from './components/Line'
+import Content from './components/Content'
+import Basic from './components/Basic'
+import { Layout } from 'antd';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="App">
+      <Sidebar />
+      <Layout className='section' >
+        <Header />
+        <Layout.Content>
+          <Line />
+          <Content />
+          <Basic />
+        </Layout.Content>
+      </Layout>
+    </Layout>
   );
 }
 
